4f9c0ddc5ec238abda767dabea24a081f4e90df7
Pranav Vashi <neobuddy89@gmail.com>
2022-11-06 12:00:08 +0530

    iconloaderlib: Force themed icon over app monochrome icon
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
+++ iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -140,10 +140,8 @@ public class IconProvider {
             icon = loadPackageIconWithFallback(info, appInfo, iconDpi);
             if (ATLEAST_T && icon instanceof AdaptiveIconDrawable && td != null) {
                 AdaptiveIconDrawable aid = (AdaptiveIconDrawable) icon;
-                if  (aid.getMonochrome() == null) {
-                    icon = new AdaptiveIconDrawable(aid.getBackground(),
-                            aid.getForeground(), td.loadPaddedDrawable());
-                }
+                icon = new AdaptiveIconDrawable(aid.getBackground(),
+                        aid.getForeground(), td.loadPaddedDrawable());
             }
         }
         return icon;
@@ -204,9 +202,6 @@ public class IconProvider {
                 Drawable drawable = resources.getDrawableForDensity(id, iconDpi, null /* theme */);
                 if (ATLEAST_T && drawable instanceof AdaptiveIconDrawable && td != null) {
                     AdaptiveIconDrawable aid = (AdaptiveIconDrawable) drawable;
-                    if  (aid.getMonochrome() != null) {
-                        return drawable;
-                    }
                     if ("array".equals(td.mResources.getResourceTypeName(td.mResID))) {
                         TypedArray ta = td.mResources.obtainTypedArray(td.mResID);
                         int monoId = ta.getResourceId(IconProvider.getDay(), ID_NULL);
@@ -214,6 +209,9 @@ public class IconProvider {
                         return monoId == ID_NULL ? drawable
                                 : new AdaptiveIconDrawable(aid.getBackground(), aid.getForeground(),
                                         new ThemeData(td.mResources, monoId).loadPaddedDrawable());
+                    }
+                    if  (aid.getMonochrome() != null) {
+                        return drawable;
                     }
                 }
                 return drawable;
