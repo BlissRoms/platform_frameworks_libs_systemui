ArrowOS-fork/android_frameworks_libs_systemui

iconloaderlib: Implement changes for icon pack support
* Extracted from 11.0 commit, adapted for A12:
  https://github.com/SuperiorOS/android_packages_apps_Launcher3/commit/6a3d407c9ff87267478846bfb7ff7174ba4d69e0

Change-Id: Ic681e884eaa983026f27f40de889756750cf3112
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: I5b71977941c844c3cd4b652ab7bf0b95a524cff7

iconloaderlib/src/com/android/launcher3/superior/icon/IconPackStore.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.superior.icon;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+public final class IconPackStore {
+    public static final String SYSTEM_ICON_PACK = "android";
+    public static final String KEY_ICON_PACK = "pref_iconPackPackage";
+
+    private Context context;
+    private SharedPreferences prefs;
+
+    public IconPackStore(Context context) {
+        this.context = context;
+        this.prefs = context.getSharedPreferences(
+                "com.android.launcher3.prefs", Context.MODE_PRIVATE);
+    }
+
+    public String getCurrent() {
+        return prefs.getString(KEY_ICON_PACK, SYSTEM_ICON_PACK);
+    }
+
+    public void setCurrent(String pkgName) {
+        prefs.edit()
+            .putString(KEY_ICON_PACK, pkgName)
+            .apply();
+    }
+
+    public boolean isUsingSystemIcons() {
+        return SYSTEM_ICON_PACK.equals(getCurrent());
+    }
+
+    public String getCurrentLabel(String defaultLabel) {
+        final String pkgName = getCurrent();
+        if (SYSTEM_ICON_PACK.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
+    }
+}

iconloaderlib/src/com/android/launcher3/superior/icon/providers/IconPackProvider.java
@@ -1,0 +1,169 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.superior.icon.providers;
+
+import android.content.Context;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.text.TextUtils;
+import android.util.ArrayMap;
+import android.util.Log;
+
+import com.android.launcher3.superior.icon.IconPack;
+import com.android.launcher3.superior.icon.IconPackStore;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class IconPackProvider {
+    private static final String TAG = "IconPackProvider";
+
+    private static Map<String, IconPack> iconPacks = new ArrayMap<>();
+
+    public static final String ICON_MASK_TAG = "iconmask";
+    public static final String ICON_BACK_TAG = "iconback";
+    public static final String ICON_UPON_TAG = "iconupon";
+    public static final String ICON_SCALE_TAG = "scale";
+
+    private IconPackProvider() {
+    }
+
+    public static IconPack getIconPack(String packageName) {
+        return iconPacks.get(packageName);
+    }
+
+    public static IconPack loadAndGetIconPack(Context context) {
+        final String packageName = new IconPackStore(context).getCurrent();
+        if (IconPackStore.SYSTEM_ICON_PACK.equals(packageName)) {
+            return null;
+        }
+        if (!iconPacks.containsKey(packageName)) {
+            loadIconPack(context, packageName);
+        }
+        return getIconPack(packageName);
+    }
+
+    public static void loadIconPack(Context context, String packageName) {
+        if (IconPackStore.SYSTEM_ICON_PACK.equals(packageName)){
+            iconPacks.put("", null);
+        }
+
+        try {
+            final XmlPullParser appFilter = getAppFilter(context, packageName);
+            if (appFilter != null) {
+                final IconPack pack = new IconPack(context, packageName);
+                parseAppFilter(packageName, appFilter, pack);
+                iconPacks.put(packageName, pack);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Invalid IconPack", e);
+        }
+    }
+
+    private static void parseAppFilter(String packageName, XmlPullParser parser,
+            IconPack pack) throws Exception {
+        final Map<String, String> iconPackResources = new HashMap<>();
+        final List<String> iconBackStrings = new ArrayList<>();
+
+        while (parser.next() != XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+
+            final String name = parser.getName();
+            if (name.equals("item")) {
+                String component = parser.getAttributeValue(null, "component");
+                final String drawable = parser.getAttributeValue(null, "drawable");
+                // Validate component/drawable exist
+                if (TextUtils.isEmpty(component) || TextUtils.isEmpty(drawable)) {
+                    continue;
+                }
+                // Validate format/length of component
+                if (!component.startsWith("ComponentInfo{") || !component.endsWith("}") ||
+                        component.length() < 16) {
+                    continue;
+                }
+                // Sanitize stored value
+                component = component.substring(14, component.length() - 1);
+                if (!component.contains("/")) {
+                    // Package icon reference
+                    iconPackResources.put(component, drawable);
+                } else {
+                    final ComponentName componentName = ComponentName.unflattenFromString(
+                            component);
+                    if (componentName != null) {
+                        iconPackResources.put(componentName.getPackageName(), drawable);
+                        iconPackResources.put(component, drawable);
+                    }
+                }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_BACK_TAG)) {
+                final String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    for (int i = 0; i < parser.getAttributeCount(); i++) {
+                        iconBackStrings.add(parser.getAttributeValue(i));
+                    }
+                }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_MASK_TAG) || name.equalsIgnoreCase(ICON_UPON_TAG)) {
+                String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        icon = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), icon);
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_SCALE_TAG)) {
+                String factor = parser.getAttributeValue(null, "factor");
+                if (factor == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        factor = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), factor);
+                continue;
+            }
+        }
+
+        pack.setIcons(iconPackResources, iconBackStrings);
+    }
+
+    private static XmlPullParser getAppFilter(Context context, String packageName) {
+        try {
+            final Resources res = context.getPackageManager()
+                    .getResourcesForApplication(packageName);
+            final int resourceId = res.getIdentifier("appfilter", "xml", packageName);
+            if (0 != resourceId) {
+                return context.getPackageManager().getXml(packageName, resourceId, null);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Failed to get AppFilter", e);
+        }
+        return null;
+    }
+}

iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -50,6 +50,8 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.os.BuildCompat;
 
+import com.android.launcher3.superior.icon.IconPack;
+import com.android.launcher3.superior.icon.providers.IconPackProvider;
 import com.android.launcher3.util.SafeCloseable;
 
 import java.util.Calendar;
@@ -123,6 +125,13 @@ public class IconProvider {
         ThemeData td = getThemeDataForPackage(packageName);
 
         Drawable icon = null;
+        try {
+            icon = getFromIconPack(packageName);
+            if (icon != null) {
+                return icon;
+            }
+        } catch (Exception e) { }
+
         if (mCalendar != null && mCalendar.getPackageName().equals(packageName)) {
             icon = loadCalendarDrawable(iconDpi, td);
         } else if (mClock != null && mClock.getPackageName().equals(packageName)) {
@@ -146,8 +155,13 @@ public class IconProvider {
     }
 
     private Drawable loadActivityInfoIcon(ActivityInfo ai, int density) {
-        final int iconRes = ai.getIconResource();
         Drawable icon = null;
+        final IconPack iconPack = IconPackProvider.loadAndGetIconPack(mContext);
+        if (iconPack != null) {
+            icon = iconPack.getIcon(ai, null, "");
+            if (icon != null) return icon;
+        }
+        final int iconRes = ai.getIconResource();
         // Get the preferred density icon from the app's resources
         if (density != 0 && iconRes != 0) {
             try {
@@ -347,5 +361,13 @@ public class IconProvider {
          * Called when the global icon state changed, which can typically affect all icons
          */
         void onSystemIconStateChanged(String iconState);
+    }
+
+    private Drawable getFromIconPack(String packageName) {
+        final IconPack iconPack = IconPackProvider.loadAndGetIconPack(mContext);
+        if (iconPack == null) {
+            return null;
+        }
+        return iconPack.getIcon(packageName, null, "");
     }
 }

BlissRoms/platform_frameworks_libs_systemui

iconloaderlib/src/com/android/launcher3/bliss/icon/IconPackStore.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.bliss.icon;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+public final class IconPackStore {
+    public static final String SYSTEM_ICON_PACK = "android";
+    public static final String KEY_ICON_PACK = "pref_iconPackPackage";
+
+    private Context context;
+    private SharedPreferences prefs;
+
+    public IconPackStore(Context context) {
+        this.context = context;
+        this.prefs = context.getSharedPreferences(
+                "com.android.launcher3.prefs", Context.MODE_PRIVATE);
+    }
+
+    public String getCurrent() {
+        return prefs.getString(KEY_ICON_PACK, SYSTEM_ICON_PACK);
+    }
+
+    public void setCurrent(String pkgName) {
+        prefs.edit()
+            .putString(KEY_ICON_PACK, pkgName)
+            .apply();
+    }
+
+    public boolean isUsingSystemIcons() {
+        return SYSTEM_ICON_PACK.equals(getCurrent());
+    }
+
+    public String getCurrentLabel(String defaultLabel) {
+        final String pkgName = getCurrent();
+        if (SYSTEM_ICON_PACK.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
+    }
+}

iconloaderlib/src/com/android/launcher3/bliss/icon/providers/IconPackProvider.java
@@ -1,0 +1,169 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.bliss.icon.providers;
+
+import android.content.Context;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.text.TextUtils;
+import android.util.ArrayMap;
+import android.util.Log;
+
+import com.android.launcher3.bliss.icon.IconPack;
+import com.android.launcher3.bliss.icon.IconPackStore;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class IconPackProvider {
+    private static final String TAG = "IconPackProvider";
+
+    private static Map<String, IconPack> iconPacks = new ArrayMap<>();
+
+    public static final String ICON_MASK_TAG = "iconmask";
+    public static final String ICON_BACK_TAG = "iconback";
+    public static final String ICON_UPON_TAG = "iconupon";
+    public static final String ICON_SCALE_TAG = "scale";
+
+    private IconPackProvider() {
+    }
+
+    public static IconPack getIconPack(String packageName) {
+        return iconPacks.get(packageName);
+    }
+
+    public static IconPack loadAndGetIconPack(Context context) {
+        final String packageName = new IconPackStore(context).getCurrent();
+        if (IconPackStore.SYSTEM_ICON_PACK.equals(packageName)) {
+            return null;
+        }
+        if (!iconPacks.containsKey(packageName)) {
+            loadIconPack(context, packageName);
+        }
+        return getIconPack(packageName);
+    }
+
+    public static void loadIconPack(Context context, String packageName) {
+        if (IconPackStore.SYSTEM_ICON_PACK.equals(packageName)){
+            iconPacks.put("", null);
+        }
+
+        try {
+            final XmlPullParser appFilter = getAppFilter(context, packageName);
+            if (appFilter != null) {
+                final IconPack pack = new IconPack(context, packageName);
+                parseAppFilter(packageName, appFilter, pack);
+                iconPacks.put(packageName, pack);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Invalid IconPack", e);
+        }
+    }
+
+    private static void parseAppFilter(String packageName, XmlPullParser parser,
+            IconPack pack) throws Exception {
+        final Map<String, String> iconPackResources = new HashMap<>();
+        final List<String> iconBackStrings = new ArrayList<>();
+
+        while (parser.next() != XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+
+            final String name = parser.getName();
+            if (name.equals("item")) {
+                String component = parser.getAttributeValue(null, "component");
+                final String drawable = parser.getAttributeValue(null, "drawable");
+                // Validate component/drawable exist
+                if (TextUtils.isEmpty(component) || TextUtils.isEmpty(drawable)) {
+                    continue;
+                }
+                // Validate format/length of component
+                if (!component.startsWith("ComponentInfo{") || !component.endsWith("}") ||
+                        component.length() < 16) {
+                    continue;
+                }
+                // Sanitize stored value
+                component = component.substring(14, component.length() - 1);
+                if (!component.contains("/")) {
+                    // Package icon reference
+                    iconPackResources.put(component, drawable);
+                } else {
+                    final ComponentName componentName = ComponentName.unflattenFromString(
+                            component);
+                    if (componentName != null) {
+                        iconPackResources.put(componentName.getPackageName(), drawable);
+                        iconPackResources.put(component, drawable);
+                    }
+                }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_BACK_TAG)) {
+                final String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    for (int i = 0; i < parser.getAttributeCount(); i++) {
+                        iconBackStrings.add(parser.getAttributeValue(i));
+                    }
+                }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_MASK_TAG) || name.equalsIgnoreCase(ICON_UPON_TAG)) {
+                String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        icon = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), icon);
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_SCALE_TAG)) {
+                String factor = parser.getAttributeValue(null, "factor");
+                if (factor == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        factor = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), factor);
+                continue;
+            }
+        }
+
+        pack.setIcons(iconPackResources, iconBackStrings);
+    }
+
+    private static XmlPullParser getAppFilter(Context context, String packageName) {
+        try {
+            final Resources res = context.getPackageManager()
+                    .getResourcesForApplication(packageName);
+            final int resourceId = res.getIdentifier("appfilter", "xml", packageName);
+            if (0 != resourceId) {
+                return context.getPackageManager().getXml(packageName, resourceId, null);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Failed to get AppFilter", e);
+        }
+        return null;
+    }
+}

iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -52,6 +52,9 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.os.BuildCompat;
 
+import com.android.launcher3.bliss.icon.IconPack;
+import com.android.launcher3.bliss.icon.providers.IconPackProvider;
+
 import com.android.launcher3.util.SafeCloseable;
 
 import com.android.launcher3.util.override.MainThreadInitializedObject;
@@ -131,6 +134,13 @@ public class IconProvider implements ResourceBasedOverride {
         ThemeData td = getThemeDataForPackage(packageName);
 
         Drawable icon = null;
+        try {
+            icon = getFromIconPack(packageName);
+            if (icon != null) {
+                return icon;
+            }
+        } catch (Exception e) { }
+
         if (mCalendar != null && mCalendar.getPackageName().equals(packageName)) {
             icon = loadCalendarDrawable(iconDpi, td);
         } else if (mClock != null && mClock.getPackageName().equals(packageName)) {
@@ -152,8 +162,13 @@ public class IconProvider implements ResourceBasedOverride {
     }
 
     private Drawable loadActivityInfoIcon(ActivityInfo ai, int density) {
-        final int iconRes = ai.getIconResource();
         Drawable icon = null;
+        final IconPack iconPack = IconPackProvider.loadAndGetIconPack(mContext);
+        if (iconPack != null) {
+            icon = iconPack.getIcon(ai, null, "");
+            if (icon != null) return icon;
+        }
+        final int iconRes = ai.getIconResource();
         // Get the preferred density icon from the app's resources
         if (density != 0 && iconRes != 0) {
             try {
@@ -353,5 +368,13 @@ public class IconProvider implements ResourceBasedOverride {
          * Called when the global icon state changed, which can typically affect all icons
          */
         void onSystemIconStateChanged(String iconState);
+    }
+
+    private Drawable getFromIconPack(String packageName) {
+        final IconPack iconPack = IconPackProvider.loadAndGetIconPack(mContext);
+        if (iconPack == null) {
+            return null;
+        }
+        return iconPack.getIcon(packageName, null, "");
     }
 }