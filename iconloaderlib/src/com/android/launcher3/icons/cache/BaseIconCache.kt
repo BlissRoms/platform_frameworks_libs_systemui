976eef1d5e31ae6976340b02ecfab324ccfcf37d
Tommy Webb <tommy@calyxinstitute.org>
2024-06-11 18:52:16 +0000

    Multiple work profile badge colors for Launcher3
    Original-Change-Id: I64f1b425eee5cc7994616050411a881d84ece99d
    Original-Change-Id-2: I9b9f01a8e483ee3de1c11a82e8b569537308596d
    Change-Id: Id1541fe766dfe12a458e42fb2c26fcb51b9003a6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/cache/BaseIconCache.kt
+++ iconloaderlib/src/com/android/launcher3/icons/cache/BaseIconCache.kt
@@ -237,10 +237,12 @@ constructor(
 
     @Synchronized
     fun getDefaultIcon(user: UserHandle): BitmapInfo {
-        if (defaultIcon == null) {
-            iconFactory.use { li -> defaultIcon = li.makeDefaultIcon(iconProvider) }
+        iconFactory.use { li ->
+            if (defaultIcon == null) {
+                defaultIcon = li.makeDefaultIcon(iconProvider)
+            }
+            return defaultIcon!!.withUser(user, li)
         }
-        return defaultIcon!!.withFlags(getUserFlagOpLocked(user))
     }
 
     protected fun getUserFlagOpLocked(user: UserHandle): FlagOp {
@@ -560,7 +562,9 @@ constructor(
             }
         }
         entry.bitmap.flags = c.getInt(INDEX_FLAGS)
-        entry.bitmap = entry.bitmap.withFlags(getUserFlagOpLocked(cacheKey.user))
+        iconFactory.use { factory ->
+            entry.bitmap = entry.bitmap.withUser(cacheKey.user, factory)
+        }
         return true
     }
 
