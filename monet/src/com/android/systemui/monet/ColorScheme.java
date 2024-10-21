2bfb19f38d84c43da5bb23dadd1a5096d9972f20
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-21 11:50:16 +0300

    monet: Allow chroma & luminance to affect secondary colors [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- monet/src/com/android/systemui/monet/ColorScheme.java
+++ monet/src/com/android/systemui/monet/ColorScheme.java
@@ -66,11 +66,12 @@ public class ColorScheme {
 
     public ColorScheme(@ColorInt int seed, boolean isDark, @Style.Type int style,
             double contrastLevel) {
-        this(seed, isDark, style, contrastLevel, 1f, 1f, false, null);
+        this(seed, isDark, style, contrastLevel, 1f, 1f, false, false, null);
     }
 
     public ColorScheme(@ColorInt int seed, boolean isDark, @Style.Type int style, double contrastLevel,
-            float luminanceFactor, float chromaFactor, boolean tintBackground, Integer bgSeed) {
+            float luminanceFactor, float chromaFactor, boolean wholePalette,
+            boolean tintBackground, Integer bgSeed) {
         this.mSeed = seed;
         this.mIsDark = isDark;
         this.mStyle = style;
@@ -124,12 +125,16 @@ public class ColorScheme {
         };
 
         mAccent1 = new TonalPalette(mMaterialScheme.primaryPalette, luminanceFactor, chromaFactor);
-        mAccent2 = new TonalPalette(mMaterialScheme.secondaryPalette);
-        mAccent3 = new TonalPalette(mMaterialScheme.tertiaryPalette);
+        mAccent2 = new TonalPalette(mMaterialScheme.secondaryPalette,
+                wholePalette ? luminanceFactor : 1f,
+                wholePalette ? chromaFactor : 1f);
+        mAccent3 = new TonalPalette(mMaterialScheme.tertiaryPalette, luminanceFactor, chromaFactor);
         mNeutral1 = new TonalPalette(bgScheme.neutralPalette,
                 tintBackground ? luminanceFactor : 1f,
                 tintBackground ? chromaFactor : 1f);
-        mNeutral2 = new TonalPalette(mMaterialScheme.neutralVariantPalette);
+        mNeutral2 = new TonalPalette(bgScheme.neutralVariantPalette,
+                tintBackground && wholePalette ? luminanceFactor : 1f,
+                tintBackground && wholePalette ? chromaFactor : 1f);
         mError = new TonalPalette(mMaterialScheme.errorPalette);
     }
 
