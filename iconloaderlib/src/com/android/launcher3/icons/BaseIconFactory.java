c4479784a0271cd3cfd33cadd7fcfb4cc09135de
Pranav Vashi <neobuddy89@gmail.com>
2024-11-28 00:44:30 +0530

    iconloaderlib: Do not wrap themed icons
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/570
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -226,18 +226,18 @@ public class BaseIconFactory implements AutoCloseable {
             // Need to convert to Adaptive Icon with insets to avoid cropping.
             tempIcon = createShapedAdaptiveIcon(bitmapDrawable.getBitmap());
         }
-        AdaptiveIconDrawable adaptiveIcon = normalizeAndWrapToAdaptiveIcon(tempIcon, null, scale);
-        Bitmap bitmap = createIconBitmap(adaptiveIcon, scale[0],
+        tempIcon = normalizeAndWrapToAdaptiveIcon(tempIcon, null, scale);
+        Bitmap bitmap = createIconBitmap(tempIcon, scale[0],
                 options == null ? MODE_WITH_SHADOW : options.mGenerationMode);
 
         int color = (options != null && options.mExtractedColor != null)
                 ? options.mExtractedColor : mColorExtractor.findDominantColorByHue(bitmap);
         BitmapInfo info = BitmapInfo.of(bitmap, color);
 
-        if (adaptiveIcon instanceof BitmapInfo.Extender extender) {
+        if (tempIcon instanceof BitmapInfo.Extender extender) {
             info = extender.getExtendedInfo(bitmap, color, this, scale[0]);
         } else if (IconProvider.ATLEAST_T && mMonoIconEnabled) {
-            Drawable mono = getMonochromeDrawable(adaptiveIcon);
+            Drawable mono = getMonochromeDrawable(tempIcon);
             if (mono != null) {
                 info.setMonoIcon(createIconBitmap(mono, scale[0], MODE_ALPHA), this);
             }
@@ -256,6 +256,14 @@ public class BaseIconFactory implements AutoCloseable {
         Drawable mono = base.getMonochrome();
         if (mono != null) {
             return new ClippedMonoDrawable(mono);
+        }
+        return null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.TIRAMISU)
+    protected Drawable getMonochromeDrawable(Drawable base) {
+        if (base instanceof AdaptiveIconDrawable) {
+            return getMonochromeDrawable((AdaptiveIconDrawable) base);
         }
         return null;
     }
@@ -324,18 +332,21 @@ public class BaseIconFactory implements AutoCloseable {
     }
 
     @Nullable
-    protected AdaptiveIconDrawable normalizeAndWrapToAdaptiveIcon(@Nullable Drawable icon,
+    protected Drawable normalizeAndWrapToAdaptiveIcon(@Nullable Drawable icon,
             @Nullable final RectF outIconBounds, @NonNull final float[] outScale) {
         if (icon == null) {
             return null;
         }
 
-        AdaptiveIconDrawable adaptiveIcon;
         float scale;
-        adaptiveIcon = wrapToAdaptiveIcon(icon, outIconBounds);
-        scale = getNormalizer().getScale(adaptiveIcon, outIconBounds, null, null);
+        if ((icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
+            AdaptiveIconDrawable adaptiveIcon;
+            adaptiveIcon = wrapToAdaptiveIcon(icon, outIconBounds);
+            icon = adaptiveIcon;
+        }
+        scale = getNormalizer().getScale(icon, outIconBounds, null, null);
         outScale[0] = scale;
-        return adaptiveIcon;
+        return icon;
     }
 
     /**
@@ -370,11 +381,7 @@ public class BaseIconFactory implements AutoCloseable {
             dr.setBounds(0, 0, 1, 1);
             boolean[] outShape = new boolean[1];
             float scale = getNormalizer().getScale(icon, outIconBounds, dr.getIconMask(), outShape);
-            if (!outShape[0] && (icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
-                // If there is an alpha on the icon, apply it to the wrapper instead.
-                dr.setAlpha(icon.getAlpha());
-                icon.setAlpha(0xFF);
-
+            if (!outShape[0]) {
                 foreground.setDrawable(createScaledDrawable(icon, scale * LEGACY_ICON_SCALE));
             } else {
                 foreground.setDrawable(createScaledDrawable(icon, 1 - getExtraInsetFraction()));
